f52ccf029e5602db2e9beccac3b9486c6d531e5c
Gunter05 <bitoukoaousmane@gmail.com>
2026-01-21 02:24:49 +0100

    fix some bug

---
+++ public/js/settings.js
@@ -0,0 +1,64 @@
+let profile= JSON.parse(localStorage.getItem("profile"));
+const loisirsIcons = {
+  sport: "fa-solid fa-dumbbell",
+  football: "fa-solid fa-futbol",
+  basketball: "fa-solid fa-basketball",
+  musique: "fa-solid fa-music",
+  danse: "fa-solid fa-person-walking",
+  cinema: "fa-solid fa-film",
+  lecture: "fa-solid fa-book",
+  ecriture: "fa-solid fa-pen",
+  voyage: "fa-solid fa-plane",
+  photographie: "fa-solid fa-camera",
+  cuisine: "fa-solid fa-utensils",
+  patisserie: "fa-solid fa-cake-candles",
+  jeux_video: "fa-solid fa-gamepad",
+  programmation: "fa-solid fa-code",
+  dessin: "fa-solid fa-paintbrush",
+  peinture: "fa-solid fa-palette",
+  chant: "fa-solid fa-microphone",
+  theatre: "fa-solid fa-masks-theater",
+  fitness: "fa-solid fa-heart-pulse",
+  yoga: "fa-solid fa-spa",
+  meditation: "fa-solid fa-leaf",
+  nature: "fa-solid fa-tree",
+  jardinage: "fa-solid fa-seedling",
+  bricolage: "fa-solid fa-hammer",
+  shopping: "fa-solid fa-bag-shopping",
+  mode: "fa-solid fa-shirt",
+  rencontre: "fa-solid fa-heart",
+  benevolat: "fa-solid fa-hands-helping"
+};
+
+
+
+document.addEventListener("DOMContentLoaded", function(){
+    initFields();
+});
+
+function initFields(){
+    document.getElementById("profile-name").innerHTML = profile.nom;
+    document.getElementById("profile-status").innerHTML = profile.genre == 'femme' ? "Etudiante" : "Etudiant";
+    document.getElementById("profile-age").innerHTML = profile.age || "";
+    document.getElementById("email").innerHTML = profile.email || "eee@ee.com";
+    document.getElementById("bio").innerHTML = profile.bio || "";
+    document.getElementById("interest-tags").innerHTML = "";
+
+    for(let interest of profile.interets){
+        document.getElementById("interest-tags").innerHTML += `
+            <div class="tag">
+                <i class='${loisirsIcons[interest]}'></i>
+                ${interest}
+            </div>`;
+    }
+    if(profile.photos.lenght >= 2){
+        for(let i=1; i < profile.photos.lenght; i++){
+            document.getElementById("gallery").innerHTML += `
+                <li>
+                    <img src="${profile.photos[i]}" alt="Photo galerie 1"/>
+                </li>`;
+        }
+    }
+
+
+}
